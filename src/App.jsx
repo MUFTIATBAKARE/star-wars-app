4f084a41e136886644be3a49a2877d8057b44671
muftiatbakare <bakare.muftiat@gmail.com>
2023-04-19 13:03:24 +0100

    update: making changes to create a pull request

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Cards from "./components/Cards";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cards = () => {
+  return <div>Star wars</div>;
+};
+
+export default Cards;
