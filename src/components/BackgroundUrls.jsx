ad1337cd950d00f218eb652842244377f27c728a
muftiatbakare <bakare.muftiat@gmail.com>
2023-04-23 22:21:51 +0100

    update: background styling

---
+++ src/components/BackgroundUrls.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const BackgroundUrls = [
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-1.0c96fd00.png",
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-2.b0a7b209.png",
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-3.1914d853.png",
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-4.c21c4d4b.png",
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-5.67d061d6.png",
+  "https://finmavis.github.io/swapi-task/static/media/bg-card-6.b4fea25d.png",
+];
+
+export default BackgroundUrls;
